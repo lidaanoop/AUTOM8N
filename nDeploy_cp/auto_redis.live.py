1e0e8a84c24081b1f27f27fa6e76c2125c5ece77
lidaanoop <lidaanoop@gmail.com>
2022-12-09 10:26:12 +0530

    testing new page

--- nDeploy_cp/auto_redis.live.py
+++ nDeploy_cp/auto_redis.live.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import cgitb
 cgitb.enable()
+def close_cpanel_liveapisock():
+    """We close the cpanel LiveAPI socket here as we dont need those"""
+    cp_socket = os.environ["CPANEL_CONNECT_SOCKET"]
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(cp_socket)
+    sock.sendall('<cpanelxml shutdown="1" />')
+    sock.close()
 close_cpanel_liveapisock()
 print('Content-Type: text/html')
 print('<div> "hello world"<div>')
